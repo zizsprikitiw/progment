fe886270b99fc8179b00850cd225aef79ae37ec8
Abdul Aziz <cengis80@gmail.com>
2019-04-04 15:41:56 +0700

    laptop_040419_1541

--- assets/custom/scripts/beranda.js
+++ assets/custom/scripts/beranda.js
@@ -40,8 +40,10 @@ var loadTaskTimeline = function() {
 			// create a dataset with items
             items = new vis.DataSet();
             items.add(data.data_task_timeline);
-			var today = new Date(new Date().setHours(0,0,0,0));
-			var tomorrow = new Date(new Date().setHours(23,59,59,999)+1);
+			// var today = new Date(new Date().setHours(0,0,0,0));
+			// var tomorrow = new Date(new Date().setHours(23,59,59,999)+1);
+			var today = new Date();
+			var tomorrow = new Date(30000 * 60 * 60 * 24 + (new Date()).valueOf());
 
             var options = {
 				orientation: 'top',
